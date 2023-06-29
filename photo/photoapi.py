176613d93fa58246cbf1097d9f894d761d808b82
XOLIXG <zlobovroma@gmail.com>
2023-06-29 21:27:46 +0500

    soc

--- photo/photoapi.py
+++ photo/photoapi.py
@@ -11,33 +11,23 @@ def get_all_photo():
 
 @photo_bp.route('/', methods=['POST'])
 def save_user_photo(user_id: int):
-    file = request.form.get('image', '')
+    file = request.files.get('image', '')
     file.save('user_images/' + file.filename)
     new_photo = post_new_photo_db(user_id, file.filename)
     return {'status': 1, 'message': 'Photo added'}
 
-@photo_bp.route('/<int:user_id>', methods=['GET'])
-def get_exact_photo(user_id: int):
-    exact_photo = get_exact_photo_db()
-    if exact_photo:
-        return {'status': 1, 'message': exact_photo}
-    return {'status': 0, 'message': 'User not found'}
 
 @photo_bp.route('/<int:photo_id>', methods=['POST'])
 def get_exact_photo(photo_id: int):
-    exact_photo = get_exact_photo_db()
+    exact_photo = get_exact_photo_db(photo_id)
     if exact_photo:
         return {'status': 1, 'message': exact_photo}
     return {'status': 0, 'message': 'User not found'}
-
-@photo_bp.route('/<int:user_id>/<int:photo_id>', methods=['PUT'])
-def change_photo(user_id: int, photo_id: int):
-    change_photo = change_photo_db()
-    return {'message': 'hello'}
+y
 
 @photo_bp.route('/<int:user_id>/<int:photo_id>',  methods=['DELETE'])
 def delete_photo(user_id: int, photo_id: int):
-    delete_user = delete_exact_photo_db()
+    delete_user = delete_exact_photo_db(photo_id)
     if delete_user:
         return {'status': 1, 'message': 'photo deleted'}
     return {'status': 0, 'message': 'photo not found'}
